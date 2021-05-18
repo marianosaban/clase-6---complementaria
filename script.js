3e99ba514b28923f728e41a4db0885139961eb57
marianosaban <75099959+marianosaban@users.noreply.github.com>
2021-05-17 21:34:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var ArrayTransmision = ["piñon fijo", "rueda libre"];
+var ArrayRuedaDel = ["triple pared", "perfil alto", "doble perfil"];
+var ArrayRuedaTras = ["triple pared", "perfil alto", "doble perfil"];
+for (let i = 0; i > ArrayRuedaDel; i++) {
+    console.log(ArrayRuedaDel[i]);
+}
+//console.log(ArrayTransmision[0]);
+//console.log(ArrayTransmision.length)
+//console.log(ArrayRuedaDel[1]);
+//console.log(ArrayRuedaTras[1])
+//var rejunteArrays = ArrayTransmision.concat(ArrayRuedaDel, ArrayRuedaTras)
+//console.log(rejunteArrays)
